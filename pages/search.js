cd7961b1b2dfb5f682be17c23354619cd4f3ca1f
ghis8 <ghislainkongolo@yahoo.com>
2022-11-14 09:36:47 +0200

    changed api key

--- pages/search.js
+++ pages/search.js
@@ -30,8 +30,8 @@ export default Search
 export async function getServerSideProps(context){
   const startIndex=context.query.start || 0
 
-  const dummyData=true
-  const data=dummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`).then(res=>res.json())
+  const dummyData=false
+  const data=dummyData ? Response : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_API_KEY}&cx=${process.env.NEXT_PUBLIC_CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`).then(res=>res.json())
   return {
     props:{
       results:data
